75ad17b08287d28efadc1f1f28b98de101043b82
Charanpal Dhanjal <charanpal@gmail.com>
2014-09-10 17:34:15 +0200

    Include all BPR code, remove useless stuff, and optimise

--- bprCython.pyx
+++ bprCython.pyx
@@ -4,65 +4,178 @@
 #cython: nonecheck=False
 
 import random 
+import logging 
+import cython
+from cython.parallel import parallel, prange
+cimport numpy
+import numpy
+from math import exp, log 
+import random
+from sandbox.util.CythonUtils cimport inverseChoice
+
+class BPRArgs(object):
+
+    def __init__(self,learning_rate=0.05,
+                 bias_regularization=1.0,
+                 user_regularization=0.0025,
+                 positive_item_regularization=0.0025,
+                 negative_item_regularization=0.00025,
+                 update_negative_item_factors=True):
+        self.learning_rate = learning_rate
+        self.bias_regularization = bias_regularization
+        self.user_regularization = user_regularization
+        self.positive_item_regularization = positive_item_regularization
+        self.negative_item_regularization = negative_item_regularization
+        self.update_negative_item_factors = update_negative_item_factors
+
+cdef class BPR(object):
+    cdef public unsigned int D, recordStep
+    cdef public double bias_regularization, user_regularization, learning_rate
+    cdef public double positive_item_regularization, negative_item_regularization
+    cdef public bint update_negative_item_factors  
+
+    def __init__(self, D, args):
+        """initialise BPR matrix factorization model
+        D: number of factors
+        """
+        self.D = D
+        self.learning_rate = args.learning_rate
+        self.bias_regularization = args.bias_regularization
+        self.user_regularization = args.user_regularization
+        self.positive_item_regularization = args.positive_item_regularization
+        self.negative_item_regularization = args.negative_item_regularization
+        self.update_negative_item_factors = args.update_negative_item_factors
+        
+        self.recordStep = 5 
+
+    def train(self, data, sampler, unsigned int num_iters):
+        """train model
+        data: user-item matrix as a scipy sparse matrix
+              users and items are zero-indexed
+        """
+        cdef unsigned int u, i, j, it       
+        cdef numpy.ndarray[double, ndim=2, mode="c"] user_factors        
+        cdef numpy.ndarray[double, ndim=2, mode="c"] item_factors        
+                
+        user_factors, item_factors, loss_samples  = self.init(data, sampler)
+        logging.debug('initial loss = {0}'.format(self.loss(loss_samples, user_factors, item_factors)))
+        
+        for it in xrange(num_iters):
+            for u, i, j in loss_samples:
+                self.update_factors(user_factors, item_factors, u, i, j)
+                
+            if it % self.recordStep == 0:
+                logging.debug( 'iteration {0}: loss = {1}'.format(it, self.loss(loss_samples, user_factors, item_factors)))
+                loss_samples = [t for t in sampler.generate_samples(data)]
+            
+        return user_factors, item_factors 
+
+    def init(self, data, sampler):
+        num_users, num_items = data.shape
+
+        user_factors = numpy.random.random_sample((num_users, self.D))
+        item_factors = numpy.random.random_sample((num_items, self.D))
+
+        loss_samples = [t for t in sampler.generate_samples(data)]
+        
+        return user_factors, item_factors, loss_samples 
+
+    def update_factors(self, numpy.ndarray[double, ndim=2, mode="c"] user_factors, numpy.ndarray[double, ndim=2, mode="c"] item_factors,  unsigned int u, unsigned int i, unsigned int j):
+        """apply SGD update"""
+        cdef bint update_j = self.update_negative_item_factors
+        cdef double x, z, d, expx
+        cdef numpy.ndarray[double, ndim=1, mode="c"] du 
+        cdef numpy.ndarray[double, ndim=1, mode="c"] di
+        cdef numpy.ndarray[double, ndim=1, mode="c"] dj
+        cdef numpy.ndarray[double, ndim=1, mode="c"] item_factors_i
+        cdef numpy.ndarray[double, ndim=1, mode="c"] item_factors_j
+        cdef numpy.ndarray[double, ndim=1, mode="c"] user_factors_u
+
+        item_factors_i = item_factors[i]
+        item_factors_j = item_factors[j]
+        user_factors_u = user_factors[u]
+
+        x = numpy.dot(user_factors_u, item_factors_i-item_factors_j)
+
+        #Fix for numerical overflow issues 
+        try: 
+            expx = exp(-x)
+            z = expx/(1.0+expx)
+        except OverflowError: 
+            if x > 0: 
+                z = 0 
+            elif x < 0: 
+                z = 1
+                
+        #Note all the regularisers are negative according to objective. Also we are adding the gradient 
+        #since we wish to maximise the objective
+        du = (item_factors_i-item_factors_j)*z - self.user_regularization*user_factors_u
+        user_factors[u, :] += self.learning_rate*du
+        
+        user_factors_u = user_factors[u, :]
+        
+        di = user_factors_u*z - self.positive_item_regularization*item_factors_i
+        item_factors[i,:] += self.learning_rate*di
+
+        dj = -user_factors_u*z - self.negative_item_regularization*item_factors_j
+        item_factors[j,:] += self.learning_rate*dj
+            
+        return du, di, dj 
+
+    def loss(self, loss_samples, numpy.ndarray[double, ndim=2, mode="c"] user_factors, numpy.ndarray[double, ndim=2, mode="c"] item_factors):
+        """
+        Compute the BPR objective which is sum_uij ln sigma(x_uij) + lambda ||theta||^2
+        """   
+        cdef unsigned int u, i, j
+        cdef double x, ranking_loss, complexity  
+        
+        ranking_loss = 0;
+        for u,i,j in loss_samples:
+            x = self.predict(user_factors, item_factors, u, i) - self.predict(user_factors, item_factors, u, j)
+            try: 
+                ranking_loss += log(1.0/(1.0+exp(-x)))
+            except OverflowError: 
+                if x > 0: 
+                    ranking_loss += log(1.0) 
+                elif x < 0: 
+                    #Really it should be minus infinity 
+                    ranking_loss += -1000
+
+        complexity = 0;
+        for u,i,j in loss_samples:
+            complexity += self.user_regularization * (user_factors[u]**2).sum()
+            complexity += self.positive_item_regularization * (item_factors[i]**2).sum()
+            complexity += self.negative_item_regularization * (item_factors[j]**2).sum()
+
+        return ranking_loss - 0.5*complexity
+
+    def predict(self, numpy.ndarray[double, ndim=2, mode="c"] user_factors, numpy.ndarray[double, ndim=2, mode="c"] item_factors, unsigned int u, unsigned int i):
+        #Note: item_basic is not in the origin paper         
+        return numpy.dot(user_factors[u], item_factors[i])
+
 
 class Sampler(object):
-
-    def __init__(self,sample_negative_items_empirically):
-        self.sample_negative_items_empirically = sample_negative_items_empirically
-
-    def init(self,data,max_samples=None):
-        self.data = data
-        self.num_users, self.num_items = data.shape
+    def __init__(self, max_samples=None):
         self.max_samples = max_samples
 
-    def sample_user(self):
-        cdef unsigned int u
-        u = self.uniform_user()
-        #num_items = self.data[u].getnnz()
-        #assert(num_items > 0 and num_items != self.num_items)
-        return u
-
-    def sample_negative_item(self,user_items):
-        cdef unsigned int j
-        
-        j = self.random_item()
-        while j in user_items:
-            j = self.random_item()
-        return j
-
-    def uniform_user(self):
-        return random.randint(0,self.num_users-1)
-
-    def random_item(self):
-        """sample an item uniformly or from the empirical distribution
-           observed in the training data
-        """
-        cdef unsigned int u, i
-        if self.sample_negative_items_empirically:
-            # just pick something someone rated!
-            u = self.uniform_user()
-            i = random.choice(self.data.rowInds(u))
-        else:
-            i = random.randint(0,self.num_items-1)
-        return i
-
     def num_samples(self, unsigned int n):
-
         if self.max_samples is None:
             return n
+            
         return min(n,self.max_samples)
 
 class UniformUserUniformItem(Sampler):
-
-    def generate_samples(self,data,max_samples=None):
-        cdef unsigned int u 
-        cdef unsigned int i, j 
+    def generate_samples(self, data):
+        cdef unsigned int u, m, n 
+        cdef unsigned int i, j
+        #cdef numpy.ndarray[unsigned int, ndim=1, mode="c"] rowInds 
+        m, n = data.shape
             
-        
-        self.init(data,max_samples)
-        for _ in xrange(self.num_samples(self.data.nnz)):
-            u = self.uniform_user()
+        for _ in xrange(self.num_samples(data.nnz)):
+            u = numpy.random.randint(0, m)
             # sample positive item
-            i = random.choice(self.data.rowInds(u))
-            j = self.sample_negative_item(self.data.rowInds(u))
-            yield u,i,j
+            rowInds = numpy.array(data.rowInds(u), dtype=numpy.uint32)
+            i = numpy.random.choice(rowInds)
+            j = inverseChoice(rowInds, n)
+
+            yield u, i, j
